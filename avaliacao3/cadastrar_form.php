4a768f4f5ceabc70b69cd6ea63268f2b631fde6f
jacksonCG <jackson.junior22@hotmail.com>
2014-01-10 12:42:24 -0200

    Create cadastrar_form.php

---
+++ avaliacao3/cadastrar_form.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+    
+        <title>Produtos</title>
+        
+    </head>
+    <body>
+        <?php
+            require_once("menu.php");
+        ?>    
+        <form action="cadastrar.php" method="post">
+            <?php require_once("campos_produtos.php"); ?>
+            <input type="submit" value="Enviar" />
+        </form>
+    </body>
+        
+</html>
