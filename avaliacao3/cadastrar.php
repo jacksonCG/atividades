439fbe5c2807281ac7daddb24ce6e293ac1aa93b
jacksonCG <jackson.junior22@hotmail.com>
2014-01-15 12:30:48 -0200

    Update cadastrar.php

--- avaliacao3/cadastrar.php
+++ avaliacao3/cadastrar.php
@@ -1,29 +1,72 @@
 <?php
     require_once("menu.php");
-
     session_start();
-    
-    $nome = $_REQUEST["nome"];
-    $cor = $_REQUEST["cor"];
-    $prazo = $_REQUEST["prazo"];
-    $detalhes = $_REQUEST["detalhes"];
-       
-    
-    $aceito = false;
-    if(isset($_REQUEST["aceito"])){
-        $aceito = true;
-    }
-
     if(!isset($_SESSION["cadastros"])){
         $_SESSION["cadastros"] = array();
         
     }
     
-    $produtos = array("nome" => $nome,"detalhes"=>$detalhes, "cor" => $cor,"aceito"=>$aceito,"prazo"=>$prazo);
+    $nome = $_REQUEST["nome"];
+    $observacoes = $_REQUEST["observacoes"];
+    $idade = $_REQUEST["idade"];
+    $estado = $_REQUEST["estado"];
+    $aceito = false;
+    if(isset($_REQUEST["aceito"])){
+        $aceito = true;
+    }
     
-    array_push($_SESSION["cadastros"],$produtos);
+    //validacao
+    $campos = true;
     
-    echo"Cadastro efetuado com sucesso !!!";
+    $observacoes = $_REQUEST["observacoes"];
+    $observacoes = trim($observacoes);
+    if(empty($observacoes)){
+        $observacoes = trim($observacoes);
+        echo"Por Favor,preencha o campo observacao <br/>";
+        $campos = false;
+    }
+    
+    if(empty($idade)){
+        echo"Por Favor,preencha o campo idade <br/>";
+        $campos = false; 
+    }
+    $nome = $_REQUEST["nome"];
+    $nome = trim($nome);
+    if(empty($nome)){
+        echo"Por Favor, preencha o campo nome <br/>";
+        $campos = false;
+    }
+    
+    if($estado == -1){
+        echo"Por Favor selecione um estado";
+        $campos = false;
+    }
+    
+    if(!isset($_REQUEST["sexo"])){
+        echo"Por Favor preencha o sexo";
+        $campos = false;
+    }
+    else{
+        $sexo = $_REQUEST["sexo"];
+    }
+    
+
+    //cadastro
+    if($campos){
+        $pessoa = array("nome" => $nome
+                         ,"sexo" => $sexo
+                         ,"idade" => $idade
+                         ,"aceito"=>$aceito
+                         ,"estado"=>$estado);
+        
+        array_push($_SESSION["cadastros"],$pessoa);
+        
+        echo"Cadastro efetuado com sucesso !!!";
+    }
+    else{
+        echo"<br/>";
+        echo"<input type='button' onclick='history.go(-1)' value='voltar' />";
+    }
     
     
     
