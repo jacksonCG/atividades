6695c7247f9186c543a34c08e15b13021cd7d0ec
jacksonCG <jackson.junior22@hotmail.com>
2014-01-17 12:22:21 -0200

    Update cadastrar.php

--- avaliacao3/cadastrar.php
+++ avaliacao3/cadastrar.php
@@ -10,6 +10,10 @@
     $observacoes = $_REQUEST["observacoes"];
     $idade = $_REQUEST["idade"];
     $estado = $_REQUEST["estado"];
+    $cpf = $_REQUEST["cpf"];
+    $telefone = $_REQUEST["telefone"];
+    
+    
     $aceito = false;
     if(isset($_REQUEST["aceito"])){
         $aceito = true;
@@ -18,32 +22,63 @@
     //validacao
     $campos = true;
     
-    $observacoes = $_REQUEST["observacoes"];
+    //CPF
+    $cpf = trim($cpf);
+    if(strlen($cpf)!=14 and strlen($cpf) !=11){
+        $cpf = trim($cpf);
+        echo"Tamanho invalido para <b>CPF</b> <br/>";
+        $campos = false;
+    }
+    
+    //TELEFONE
+    $telefone = trim($telefone);
+    if(strlen($telefone)!=8){
+        echo"Tamanho invalido para <b>TELEFONE</b> <br/>";
+        $campos = false;
+    }
+    else if(!ctype_digit($telefone)){
+        echo"Campo Telefone somente com <b>NUMERO</b> <br/>";
+        $campos = false;
+    }
+    
+    //OBSERVAÇÃO
     $observacoes = trim($observacoes);
     if(empty($observacoes)){
-        $observacoes = trim($observacoes);
-        echo"Por Favor,preencha o campo observacao <br/>";
+        echo"Por Favor,preencha o campo <b>OBSERVAÇÃO</b> <br/>";
+        $campos = false;
+    }
+    else if(!ctype_alnum($observacoes)){
+        echo"Campo observacao somente com <b>LETRAS & NUMEROS </b> <br/>";
         $campos = false;
     }
     
+    //IDADE
+    $idade = trim($idade);
     if(empty($idade)){
-        echo"Por Favor,preencha o campo idade <br/>";
+        echo"Por Favor,preencha o campo <b>IDADE</b> <br/>";
         $campos = false; 
     }
-    $nome = $_REQUEST["nome"];
+    
+    //NOME
     $nome = trim($nome);
     if(empty($nome)){
-        echo"Por Favor, preencha o campo nome <br/>";
+        echo"Por Favor, preencha o campo <b>NOME</b> <br/>";
+        $campos = false;
+    }
+    else if(!ctype_alpha($nome)){
+        echo"Campo Nome somente com <b>LETRAS</b> <br/>";
         $campos = false;
     }
     
+    //ESTADO
     if($estado == -1){
-        echo"Por Favor selecione um estado";
+        echo"Por Favor selecione um <b>ESTADO</b>";
         $campos = false;
     }
     
+    //SEXO
     if(!isset($_REQUEST["sexo"])){
-        echo"Por Favor preencha o sexo";
+        echo"Por Favor preencha o <b>SEXO</b>";
         $campos = false;
     }
     else{
@@ -57,11 +92,13 @@
                          ,"sexo" => $sexo
                          ,"idade" => $idade
                          ,"aceito"=>$aceito
-                         ,"estado"=>$estado);
+                         ,"estado"=>$estado
+                         ,"cpf"=>$cpf
+                         ,"telefone"=>$telefone);
         
         array_push($_SESSION["cadastros"],$pessoa);
         
-        echo"Cadastro efetuado com sucesso !!!";
+        echo"Cadastro efetuado com SUCESSO !!!";
     }
     else{
         echo"<br/>";
